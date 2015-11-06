8db32b11c8815085fca83695725bbfbc65048449
Anthony Van de Gejuchte <anthonyvdgent@gmail.com>
2015-11-06 13:56:50 +0100

    Attempt to sort results by matching score

--- src/irail/stations/Stations.php
+++ src/irail/stations/Stations.php
@@ -9,6 +9,19 @@
 
 namespace irail\stations;
 
+$binarySearchInternal = function($input, $value, $cmp, $min, $max) {
+    while ($min !== $max) {
+        $piv = ($min + $max) >> 1;
+        if ($cmp($input[$piv], $value) > 0) {
+            $max = $piv;
+        } else {
+            $min = $piv + 1;
+        }
+    }
+
+    return $min;
+};
+
 class Stations
 {
     private static $stationsfilename = '/../../../stations.jsonld';
@@ -27,6 +40,7 @@ class Stations
     public static function getStations($query = '', $country = '')
     {
         if ($query && $query !== '') {
+            $length = 5;
             // Filter the stations on name match
             $stations = json_decode(file_get_contents(__DIR__.self::$stationsfilename));
             $newstations = new \stdClass();
@@ -52,45 +66,61 @@ class Stations
             // st. is the same as Saint
             $query = str_ireplace('st-', 'st ', $query);
             $query = str_ireplace('st.-', 'st ', $query);
-            $query = preg_replace("/st(\s|$|\.)/i", '(saint|st|sint) ', $query);
+            $query = preg_replace("/st(\s|$|\.)/i", 'saint ', $query);
             //make sure that we're only taking the first part before a /
             $query = explode('/', $query);
             $query = trim($query[0]);
 
             // Dashes are the same as spaces
             $query = self::normalizeAccents($query);
-            $query = str_replace("\-", "[\- ]", $query);
-            $query = str_replace(' ', "[\- ]", $query);
+            $query = str_replace("\-", " ", $query);
 
             $count = 0;
 
-            // Create a sorted list based on the vehicle_frequency
-            $stations_array = $stations->{'@graph'};
-            usort($stations_array, ['\irail\stations\Stations','cmp_stations_vehicle_frequency']);
+            foreach($stations_array as $stationName) {
+                $station = str_replace("-", " ", $stationName); // Dashes are the same as spaces
+                $station = str_ireplace("sint ", "saint ", $stationName); // sint is the same as saint
+                $pass = false;
 
-            foreach ($stations_array as $station) {
-                if (preg_match('/.*'.$query.'.*/i', str_replace(' am ', ' ', self::normalizeAccents($station->{'name'})), $match)) {
-                    $newstations->{'@graph'}[] = $station;
-                    $count++;
-                } elseif (isset($station->alternative)) {
-                    if (is_array($station->alternative)) {
-                        foreach ($station->alternative as $alternative) {
-                            if (preg_match('/.*('.$query.').*/i', str_replace(' am ', ' ', self::normalizeAccents($alternative->{'@value'})), $match)) {
-                                $newstations->{'@graph'}[] = $station;
-                                $count++;
-                                break;
-                            }
+                // Get position of query in station
+                $pos = stripos($station, $query);
+
+                // We have a full match
+                if ($pos >= 0) {
+                    $pass = ['station' => $station, 'match' => [
+                        'full' => $pos, 'matches' => [
+                            ['match' => $query, 'pos' => $pos, 'length' => sizeof($query)]
+                        ]
+                    ]];
+                }
+
+                // Add item to results
+                if ($false !== false) {
+                    if (sizeof($stations->{'@graph'}) > $length) {
+                        $stations->{'@graph'}[] = $pass;
+
+                        if (sizeof($stations->{'@graph'}) === $length) {
+                            $stations->{'@graph'} = usort($stations->{'@graph'}, self::cmp_stations_relevance);
                         }
                     } else {
-                        if (preg_match('/.*'.$query.'.*/i', str_replace(' am ', ' ', self::normalizeAccents($alternative->{'@value'})))) {
-                            $newstations->{'@graph'}[] = $station;
-                            $count++;
+                        // Check if station made it to the top list
+                        $result = self::cmp_stations_relevance($stations->{'@graph'}[$length], $false);
+
+                        // Insert item if it made to the list
+                        if ($result < 0) {
+                            $insertPos = $binarySearchInternal($result->{'@graph'}, $pass, self::cmp_stations_relevance, 0, sizeof($result->{'@graph'}));
+                            $result->{'@graph'} = array_splice($result->{'@graph'}, $insertPos, 0, $pass);
                         }
                     }
                 }
-                if ($count > 5) {
-                    return $newstations;
-                }
+            }
+
+            // Trim results to required length
+            $result->{'@graph'} = array_slice($result->{'@graph'}, 0, $length);
+
+            // Remove matches
+            foreach ($result->{'@graph'} as $graph) {
+                unset($graph->match);
             }
 
             return $newstations;
@@ -114,6 +144,60 @@ class Stations
         }
         //sort sorts from low to high, so lower avgStopTimes will result in a higher ranking.
         return ($a->avgStopTimes < $b->avgStopTimes) ? 1 : -1;
+    }
+
+    /**
+     * Compare 2 stations based on query match score
+     *
+     * @param $a Object the first station
+     * @param $b Object the second station
+     *
+     * @return int The result of the compare. 0 if equal, -1 if a is after b, 1 if b is before a
+     */
+    public static function cmp_stations_relevance($a, $b) {
+
+        if (!is_object($a) || !is_object($a->{'@graph'}) || !is_array($a->{'@graph'}["match"])) {
+
+            if (!is_object($b) || !is_object($b->{'@graph'}) || !is_array($b->{'@graph'}["match"])) {
+                return this::cmp_stations_vehicle_frequency($a, $b);
+            } else {
+                return -1;
+            }
+        }
+
+        if (!is_object($b) || !is_object($b->{'@graph'}) || !is_array($b->{'@graph'}["match"])) {
+            return 1;
+        }
+
+        $pos = 0;
+        while($a->{'@graph'}["match"][$pos] || $b->{'@graph'}["match"][$pos]) {
+            // Any match is better than no match
+            if (!is_object($a->{'@graph'}["match"][$pos])) {
+                if ($a->{'@graph'}["match"][$pos]) {
+                    break; // Tie
+                }
+
+                return 1;
+            } else if (!is_object($b->{'@graph'}["match"][$pos])) {
+                return -1;
+            }
+
+            // Earlier matches have bigger priority
+            if ($a->{'@graph'}["match"][$pos]['pos'] < $b->{'@graph'}["match"][$pos]['pos']) {
+                return -1;
+            } else if ($a->{'@graph'}["match"][$pos]['pos'] > $b->{'@graph'}["match"][$pos]['pos']) {
+                return 1;
+            }
+
+            // Bigger matches have bigger priority
+            if ($a->{'@graph'}["match"][$pos]['length'] > $b->{'@graph'}["match"][$pos]['length']) {
+                return -1;
+            } else if ($a->{'@graph'}["match"][$pos]['pos'] < $b->{'@graph'}["match"][$pos]['length']) {
+                return 1;
+            }
+        }
+
+        return this::cmp_stations_vehicle_frequency($a, $b);
     }
 
     /**
